414b3a5ec9e40e2bd59061c88d45ece1f72a9491
Tharles Claysson de Oliveira Sanches <tharlesco.sanches@gmail.com>
2021-08-25 08:25:21 -0300

    atualizar 25/08
    terminando lição produto

--- ArrayProduto/Program.cs
+++ ArrayProduto/Program.cs
@@ -10,20 +10,35 @@ namespace ArrayProduto
     {
         static void Main(string[] args)
         {
-            /* Crie um vetor de 3 elementos
-             * Insira/digite os valores dos atributos
-             * 
-             * abra outro laço de repetição, aplique um percentual
-             * de aumento aos produtos
-             * 
-             * apresente ao final qual é a diferença entre o total dos preços
-             * antigos e após o aumento
-             * 
-             * Apenas use Um vetor
-             * 
-             * Apresente os atributos após o aumento
-             * 
-             */
+            Produto[] vetProd = new Produto[3];
+            double soma_preco_antigo = 0;
+            for (int i = 0; i< vetProd.Length; i++)
+            {
+                vetProd[i] = new Produto();
+                Console.Write("DIgite o código do produto:");
+                vetProd[i].codigo = Convert.ToInt32(Console.ReadLine());
+                Console.Write("DIgite o tipo de produto:");
+                vetProd[i].descricao = Console.ReadLine();
+                Console.Write("DIgite o preço do produto:");
+                vetProd[i].preco = Convert.ToInt32(Console.ReadLine());
+                soma_preco_antigo += vetProd[i].preco;
+            }
+            for (int i = 0; i < vetProd.Length; i++)
+                vetProd[i].Mostrar();
+            double soma_preco_novo = 0;
+            Console.Write("\nQual é o percentual de aumento dos produtos?: ");
+            double percentual = Convert.ToDouble(Console.ReadLine());
+            foreach (Produto p in vetProd)
+            {
+                p.preco = p.preco + (p.preco * percentual / 100);
+                soma_preco_novo += p.preco;
+                p.Mostrar();
+            }
+            double diferenca = soma_preco_novo - soma_preco_antigo;
+            Console.WriteLine("\nSoma dos preços antes do aumento: " + soma_preco_antigo);
+            Console.WriteLine("Soma dos preços após o aumento: " + soma_preco_novo);
+            Console.WriteLine("A diferença entre os preços foi de R$: " + diferenca);
+            Console.ReadKey();
         }
     }
 }
